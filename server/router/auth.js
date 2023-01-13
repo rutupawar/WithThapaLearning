e560eeaa96bfe1fe767bc87efdbec736e95ad39a
Rishi <rutupawar999@gmail.com>
2023-01-13 11:22:18 +0530

    promises vs async

--- server/router/auth.js
+++ server/router/auth.js
@@ -6,32 +6,59 @@ router.get('/', (req, res) => {
     res.send('hit at from auth.js');
 });
 
-router.get('/register', (req, res) => {
+// Below implementation is using promises
+// router.post('/register', (req, res) => {
+//     const {name, email, phone, password, cpassword} = req.body;
+
+//     if(!name || !email || !phone || !password || !cpassword){
+//         // Writing return here is important, else it would try to send responce, when its already sent.
+//         return res.status(422).json({      // Setting same as msg. But it responded this. Why ?
+//             error: "Send all required fields"
+//         });
+//     }
+
+//     User.findOne({email: email }).then((userExist) => {
+//         if(userExist){
+//             return res.status(422).json({ error: "Email already exists" });
+//         }
+//         const user = new User({name, email, phone, password, cpassword });
+
+//         user.save().then(()=>{
+//             res.status(201).json({ message: "User registed successfully"});
+//         }).catch((err)=>{
+//             res.status(500).json({ error: "Failed to register" });
+//         });
+//     }).catch((err)=>{
+//         console.log(err);
+//     });
+
+// });
+
+// Now by using async
+
+router.post('/register', async (req, res) => {
     const {name, email, phone, password, cpassword} = req.body;
 
     if(!name || !email || !phone || !password || !cpassword){
-        // Writing return here is important, else it would try to send responce, when its already sent.
-        return res.status(422).json({      // Setting same as msg. But it responded this. Why ?
+        return res.status(422).json({
             error: "Send all required fields"
         });
     }
 
-    User.findOne({email: email }).then((userExist) => {
+    try{
+        const userExist = await User.findOne({email: email });
         if(userExist){
-            // Return here is important
             return res.status(422).json({ error: "Email already exists" });
         }
+
         const user = new User({name, email, phone, password, cpassword });
+        await user.save();
 
-        user.save().then(()=>{
-            res.status(201).json({ message: "User registed successfully"});
-        }).catch((err)=>{
-            res.status(500).json({ error: "Failed to register" });
-        });
-    }).catch((err)=>{
+        res.status(201).json({ message: "User registed successfully" });
+
+    } catch ( err ) {
         console.log(err);
-    });
-
+    }
 });
 
 module.exports = router;
