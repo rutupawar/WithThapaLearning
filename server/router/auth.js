5583cb351b7dcfb3d0b1b7e083237ed142081823
Rishi <rutupawar999@gmail.com>
2023-01-14 20:30:16 +0530

    basic login without hashing password

--- server/router/auth.js
+++ server/router/auth.js
@@ -6,36 +6,6 @@ router.get('/', (req, res) => {
     res.send('hit at from auth.js');
 });
 
-// Below implementation is using promises
-// router.post('/register', (req, res) => {
-//     const {name, email, phone, password, cpassword} = req.body;
-
-//     if(!name || !email || !phone || !password || !cpassword){
-//         // Writing return here is important, else it would try to send responce, when its already sent.
-//         return res.status(422).json({      // Setting same as msg. But it responded this. Why ?
-//             error: "Send all required fields"
-//         });
-//     }
-
-//     User.findOne({email: email }).then((userExist) => {
-//         if(userExist){
-//             return res.status(422).json({ error: "Email already exists" });
-//         }
-//         const user = new User({name, email, phone, password, cpassword });
-
-//         user.save().then(()=>{
-//             res.status(201).json({ message: "User registed successfully"});
-//         }).catch((err)=>{
-//             res.status(500).json({ error: "Failed to register" });
-//         });
-//     }).catch((err)=>{
-//         console.log(err);
-//     });
-
-// });
-
-// Now by using async
-
 router.post('/register', async (req, res) => {
     const {name, email, phone, password, cpassword} = req.body;
 
@@ -60,5 +30,27 @@ router.post('/register', async (req, res) => {
         console.log(err);
     }
 });
+
+router.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password){
+        return res.status(400).json({"error": "Fill all fields"});
+    }
+
+    try{
+        const userLogin = await User.findOne({ email: email});
+        console.log(userLogin);
+        if(userLogin){
+            res.json({"success": "Login done"});
+        }
+        else{
+            res.json({"fail": "Login failed"});
+        }
+        // res.json({"succes1s": "Login done"});        // res.json sends responce directly
+    }
+    catch{
+        console.log(err);
+    }
+})
 
 module.exports = router;
